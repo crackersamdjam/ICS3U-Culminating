0b6bd902e50d1b188c3396facc13a0c3fc0e88e7
mbagrianski <46272573+mbagrianski@users.noreply.github.com>
2018-12-30 21:58:59 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,29 +10,29 @@ import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    
+    Static Board board = new Board();
     @Override
     public void start(Stage primaryStage) {
         GridPane root = new GridPane();
-        final int size = 8 ;
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col ++) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j ++) {
                 StackPane square = new StackPane();
-                String color ;
-                if ((row + col) % 2 == 0) {
-                    color = "beige";
+                String colour ;
+                if ((i + j) % 2 == 0) {
+                    colour = "beige";
                 } else {
-                    color = "brown";
+                    colour = "brown";
                 }
-                square.setStyle("-fx-background-color: "+color+";");
-                root.add(square, col, row);
+                square.setStyle("-fx-background-color: "+colour+";");
+                root.add(square, j, i);
             }
         }
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < 8; i++) {
             root.getColumnConstraints().add(new ColumnConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, HPos.CENTER, true));
             root.getRowConstraints().add(new RowConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, VPos.CENTER, true));
         }
-        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
